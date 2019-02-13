891ebf66feda3fecf6d315128b7b27bdf0be7bfb
Khushboo Sakervala <ksakerva@my.centennialcollege.ca>
2019-02-13 02:23:22 -0500

    added bootstrap

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,12 @@
 /* Custom JavaScript */
+//IIFE
+(function() {
+  function Start() {
+    console.log(
+      `%c App Started.......`,
+      "font-size:20 px; color: blue; font-weight:bold"
+    );
+  }
+
+  window.addEventListener("load", Start);
+})();
