5d3f3ce9d7ddcd0de7fef89bcc6bbacbc104eb56
Khushboo Sakervala <ksakerva@my.centennialcollege.ca>
2019-02-14 01:30:47 -0500

    about me page updated

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,22 @@ router.get("/", function(req, res, next) {
 
 /* GET about page. */
 router.get("/about", function(req, res, next) {
-  res.render("index", { title: "About" });
+  res.render("about/index", { title: "About" });
 });
 
 /* GET projects page. */
 router.get("/projects", function(req, res, next) {
-  res.render("index", { title: "Projects" });
+  res.render("projects/index", { title: "Projects" });
 });
 
 /* GET services page. */
 router.get("/services", function(req, res, next) {
-  res.render("index", { title: "Services" });
+  res.render("services/index", { title: "Services" });
 });
 
 /* GET contact page. */
 router.get("/contact", function(req, res, next) {
-  res.render("index", { title: "Contact" });
+  res.render("contact/index", { title: "Contact" });
 });
 
 module.exports = router;
