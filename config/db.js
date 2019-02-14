5365f38b60e20956241c1fdbc92dd208d149d7d8
Khushboo Sakervala <ksakerva@my.centennialcollege.ca>
2019-02-13 23:49:51 -0500

    added custom logo

---
+++ config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // local MongoDB deployment
+  //URI: "mongodb://localhost/khushboo"
+  //URI : "mongodb://localhost/demo"
+  //URI: "mongodb://<dbuser>:<dbpassword>@ds119755.mlab.com:19755/week4"
+};
